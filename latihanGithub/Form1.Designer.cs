202e67fc6dbda5298d5edd5073a4d5caf5613a1e
rasyasultanpasha-ship-it <rasyasultanpasha@gmail.com>
2025-10-07 14:48:40 +0800

    digawi

--- latihanGithub/Form1.Designer.cs
+++ latihanGithub/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.siswaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jurusanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.siswaToolStripMenuItem,
+            this.jurusanToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(73, 29);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // siswaToolStripMenuItem
+            // 
+            this.siswaToolStripMenuItem.Name = "siswaToolStripMenuItem";
+            this.siswaToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.siswaToolStripMenuItem.Text = "Siswa";
+            // 
+            // jurusanToolStripMenuItem
+            // 
+            this.jurusanToolStripMenuItem.Name = "jurusanToolStripMenuItem";
+            this.jurusanToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.jurusanToolStripMenuItem.Text = "Jurusan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem siswaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jurusanToolStripMenuItem;
     }
 }
 
