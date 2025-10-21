266af67e23f1a7c995984e7709f0e29597717715
fadhil49 <jabluknadir@gmail.com>
2025-10-21 12:58:06 +0700

    btnsiswa

--- latihanGithub/FormSiswa.Designer.cs
+++ latihanGithub/FormSiswa.Designer.cs
@@ -118,7 +118,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(167, 295);
+            this.button1.Location = new System.Drawing.Point(182, 295);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 31);
             this.button1.TabIndex = 9;
